1d3e1ffd42b3c1e256445fb87d7ae1e6d85e20bc
aprilkrgonzales <april.gonzales@generalassemb.ly>
2021-11-30 17:14:21 -0700

    added in calcuate button so math is preformed only once the equal button is clicked

--- react_calulator/src/App.js
+++ react_calulator/src/App.js
@@ -5,6 +5,7 @@ function App() {
 
   const [firstNum, setFirstNum] = useState("");
   const [secondNum, setSecondNum] = useState("");
+  const [operator, setOperator] = useState(null);
   const [result, setResult] = useState(0);
 
   const handleChange = (e) => {
@@ -29,15 +30,47 @@ function App() {
       <h1>
         React Calculator
       </h1>
-      <form onSubmit={ handleChange }>
-        <input type="text" name="firstNum" value={ firstNum } placeholder="number" onChange={ (e) => setFirstNum(e.target.value) } />
-        <input type="text" name="secondNum" value={ secondNum } placeholder="number" onChange={ (e) => setSecondNum(e.target.value) } />
+      <form onSubmit={handleChange}>
+        <input type="text" name="firstNum" value={firstNum} placeholder="number" onChange={(e) => setFirstNum(e.target.value)} />
+        <input type="text" name="secondNum" value={secondNum} placeholder="number" onChange={(e) => setSecondNum(e.target.value)} />
       </form>
-          <button onClick={ () => { handleAdd() } }>+</button>
-          <button onClick={ () => { handleSubtract() } }>-</button>
-          <button onClick={ () => { handleMultiply() } }>*</button>
-          <button onClick={ () => { handleDivide() } }>/</button>
-      <p>= { result }</p>
+      <button onClick={() => { setOperator("+") }}>+</button>
+      <button onClick={() => { setOperator("-") }}>-</button>
+      <button onClick={() => { setOperator("*") }}>*</button>
+      <button onClick={() => { setOperator("/") }}>/</button>
+
+      <form onSubmit={handleChange}>
+        {
+          operator === "+" ?
+          <button onClick={() => { handleAdd() }}>=</button>
+            :
+            null
+        }
+        {
+          operator === "-" ?
+            <button onClick={() => { handleSubtract() }}>=</button>
+            :
+            null
+        }
+        {
+          operator === "*" ?
+            <button onClick={() => { handleMultiply() }}>=</button>
+            :
+            null
+        }
+        {
+          operator === "/" ?
+            <button onClick={() => { handleDivide() }}>=</button>
+            :
+            null
+        }
+      </form>
+        {
+          result ?
+          <p> = {result}</p>
+          :
+          null
+        }
     </div>
   );
 }
